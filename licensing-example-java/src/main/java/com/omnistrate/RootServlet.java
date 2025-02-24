bf6536a60f4b48f2d7061220bce4ecbb3a6cf14a
pberton <1789738+pberton@users.noreply.github.com>
2025-02-23 22:01:58 -0800

    java 21

--- licensing-example-java/src/main/java/com/omnistrate/RootServlet.java
+++ licensing-example-java/src/main/java/com/omnistrate/RootServlet.java
@@ -8,22 +8,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.omnistrate.licensing.validation.Validator;
 
-@WebServlet("/")
+@WebServlet("/license")
 public class RootServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-
-
-        // Validate license key for product
-        // This is the validation method that can be used to validate license key for a product in Omnistrate
-        // With a simple call to this method, you can:
-        // - confirm the validity of the certificate that signed the license
-        // - validate the license signature
-        // - validate the license expiration date
-        // - validate the unique sku configured in omnistrate maps with the product
-        // - validate that the injected variable containing the instance-id maps with the license
         try {
+            // Validate license key for product
+            // This is the validation method that can be used to validate license key for a product in Omnistrate
+            // With a simple call to this method, you can:
+            // - confirm the validity of the certificate that signed the license
+            // - validate the license signature
+            // - validate the license expiration date
+            // - validate the unique sku configured in omnistrate maps with the product
+            // - validate that the injected variable containing the instance-id maps with the license
             boolean isValid = Validator.validateLicenseForProduct("PRODUCT-SAMPLE-JAVA-SKU-UNIQUE-VALUE"); 
             if (isValid) {
                 response.getWriter().println("<h1>Success</h1><p>License is valid</p>");
@@ -34,7 +32,6 @@ public class RootServlet extends HttpServlet {
         } catch (Exception e) {
             response.getWriter().println("<h1>Error</h1><p>" + e.getMessage() + "/p>");
         }
-        response.getWriter().println("<h1>Welcome to the Root Path!</h1>");
     }
 }
 
