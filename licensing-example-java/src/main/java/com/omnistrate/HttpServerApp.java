70ff849667cd2ab03090a0d67145135dc277f34c
pberton <1789738+pberton@users.noreply.github.com>
2025-02-24 08:29:44 -0800

    move out of jetty (#3)

---
+++ licensing-example-java/src/main/java/com/omnistrate/HttpServerApp.java
@@ -0,0 +1,42 @@
+package com.omnistrate;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.omnistrate.licensing.validation.Validator;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class HttpServerApp {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Starting HTTP server on port 8080");
+        // Create a simple HTTP server
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new RootHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class RootHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "";
+            try {
+                boolean isValid = Validator.validateLicenseForProduct("PRODUCT-SAMPLE-JAVA-SKU-UNIQUE-VALUE"); 
+                if (isValid) {
+                    response = "<h1>Success</h1><p>License is valid</p>";
+                } else {
+                    response = "<h1>Error</h1><p>License is invalid/p>";
+                }
+            } catch (Exception e) {
+                response = "<h1>Error</h1><p>" + e.getMessage() + "/p>";
+            }
+
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
